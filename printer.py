d69b7579fa07ffa433d91cfab7aa84e385e89c1e
Martin Průša <martinprusa@Martin-iMac-24.local>
2024-12-30 16:36:31 +0100

    printer documentation

--- printer.py
+++ printer.py
@@ -1,14 +1,39 @@
 class ConsolePrinter:
+    """
+        Třída pro výpis informací
+    """
+
     def print(self, message):
+        """
+            Vytiskne textovou zprávu
+            Parametry:
+                message - string
+        """
         print(message)
 
     def print_line(self):
+        """
+            Vytiskne čáru
+        """
         print('-'*50)
 
     def print_highlighted(self, message):
+        """
+            Vytiskne textovou zprávu tučně
+            Parametry:
+                message - string
+        """
         print(f"\033[1m{message}\033[0m")
 
     def print_game(self, game):
+        """
+            Vytsikne průběh hry (historii hádaných kombinací)
+            Parametry:
+                game - list of tuples
+                        - tuple (list of int, tuple of int)
+                            - list of int - hádaná kombinace
+                            - tuple of int - počet správných pozic a správných barev
+        """
         for round_number, guess in enumerate(game):
             print(f"{round_number:2}: ", end='')
             for color in guess[0]:
